05b42d2dbf6ab8d0494a1041b5a500aa3d7e5364
Mohammad Akbari <mohammad.a@nizek.com>
2024-07-13 12:06:33 +0330

    Initial Commit

---
+++ Server_Driven_UI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Server_Driven_UI
+//
+//  Created by Mohammad Akbari on 7/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Server_Driven_UI/Server_Driven_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Server_Driven_UIApp.swift
+//  Server_Driven_UI
+//
+//  Created by Mohammad Akbari on 7/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Server_Driven_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
